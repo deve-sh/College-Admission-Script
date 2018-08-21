f112d0bf14b38df61c2b423f8007416885958dd3
Devesh Kumar <devesh2027@gmail.com>
2018-08-21 21:32:01 +0530

    Add files via upload

---
+++ inc/style.php
@@ -0,0 +1,9 @@
+<?
+?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/smallscreen.css" media="screen and (max-width: 1000px)">
+	<link rel="stylesheet" type="text/css" href="css/ultrasmall.css" media="screen and (max-width:600px)">
+	<link rel="stylesheet" type="text/css" href="css/print.css" media="print">
+<?
+?>
